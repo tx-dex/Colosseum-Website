99a18ba5cf88404cc84358bb3deb04b87e693b00
Antti Malinen <antti.malinen@sangre.fi>
2020-12-02 15:07:11 +0200

    Find a clinic & newsroom

---
+++ static/page-find-a-clinic.php
@@ -0,0 +1,91 @@
+<?php require 'region-header.php'; ?>
+
+<nav role="navigation" class="navbar navbar-sub">
+    <div class="container-fullwidth">
+        <ul class="nav navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link " href="/leading-dentistry/medical-governance">Medical Governance</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" href="/leading-dentistry/medical-governance">Tässä toinen alasivu</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<nav aria-label="breadcrumb">
+    <div class="container-fullwidth">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="#">Newsroom</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Library</li>
+        </ol>
+    </div>
+</nav>
+
+<section class="hero-basic-page with-image">
+    <div class="container-fullwidth">
+        <div class="hero-content">
+            <h1>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.
+            </h1>
+        </div>
+    </div>
+    <div class="hero-img">
+        <img src="img/hero-basic-page-example.jpg" alt="">
+    </div>
+</section>
+
+<div class="page-sections">
+    <section class="section-main-content">
+        <div class="main-content-container">
+            <div class="container-fullwidth">
+
+                <h2>Find your local clinic:</h2>
+
+                <div class="article-list country-list">
+                    <a href="#" class="item mb-4">
+                        <div class="col-img">
+                            <div class="square-wrapper">
+                                <div class="sw-content">
+                                    <img src="img/country-logos/logo-colosseum-dental-uk.jpg" alt="UK">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-txt">
+                            <h3 class="article-title">Denmark: Colosseum Tandlæge</h3>
+                            <div class="article-lead">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                            </div>
+                            <button class="btn btn-link add-fwd-icon-before">https://www.colosseumdental.co.uk</button>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="article-list">
+                    <a href="#" class="item mb-4">
+                        <div class="col-date">
+                            <div class="square-wrapper">
+                                <div class="sw-content">
+                                    <div class="date">
+                                        <span class="day">24</span>
+                                        <span class="month">December</span>
+                                        <span class="year">2020</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-txt">
+                            <span class="category-badge">Press release</span>
+                            <h3 class="article-title">The Colosseum Dental Group Academy opens with a new digital curriculum</h3>
+                            <button class="btn btn-link add-fwd-icon">Read more</button>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+
+<?php require 'region-footer.php'; ?>
+
